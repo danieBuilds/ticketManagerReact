886780f6c8a5d6aca4a4da461eca3cbc9715157c
danieBuilds <daniemodu@gmail.com>
2025-10-27 18:27:20 +0100

    base edit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ticketManagementReact/',
+  base: '/ticketManagerReact/',
   plugins: [react()],
   build: {
     outDir: 'dist',
